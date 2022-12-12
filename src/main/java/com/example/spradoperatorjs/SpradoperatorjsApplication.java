cf707d73a43c42f453f243a5be0b4621553d5db6
formula38 <louismconley@gmail.com>
2022-12-12 08:27:51 -0800

    Initial commit

---
+++ src/main/java/com/example/spradoperatorjs/SpradoperatorjsApplication.java
@@ -0,0 +1,13 @@
+package com.example.spradoperatorjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpradoperatorjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpradoperatorjsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spradoperatorjs/SpradoperatorjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spradoperatorjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpradoperatorjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
